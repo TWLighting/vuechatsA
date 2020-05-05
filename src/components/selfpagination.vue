5636e725bf16a7474eed0b0738435479ae37dfe5
jim_apacer <jim_apacer@gmail.com>
2020-05-05 16:21:21 +0800

    update working

---
+++ src/components/selfpagination.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav class="d-flex justify-content-center">
+    <ul class="pagination nostyle">
+      <li class="pagination-btn first-btn"></li>
+      <li class="pagination-btn previous-btn"></li>
+      <li v-for="index of total">
+        <span>{{index}}</span>
+      </li>
+      <li class="pagination-btn next-btn"></li>
+      <li class="pagination-btn last-btn"></li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  model: {
+    prop: "currentPage",
+    event: "click"
+  },
+  props: {
+    currentPage: {
+      type: Number,
+      default: null
+    },
+    totalRows: {
+      type: Number,
+      default: null
+    },
+    perPage: {
+      type: Number,
+      default: null
+    }
+  },
+  computed: {
+    total: function() {
+      let pages = Math.ceil(this.totalRows / this.perPage);
+      let arr = [];
+      for (let i = 1; i <= pages; i++) {
+        arr.push(i);
+      }
+      return arr;
+    }
+  }
+};
+</script>
---
+++ src/components/widgetCoreanalyzer.vue
@@ -0,0 +1,223 @@
+<template>
+  <div>
+    <b-row class="d-flex">
+      <div class="piereadgroup" v-for="(option,index) in ssdoptions" :key="index">
+        <Piechart :chartData="option.chartData" :options="option.chartOptions"></Piechart>
+      </div>
+    </b-row>
+  </div>
+</template>
+<script>
+import Piechart from "./charts/peichart";
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: null
+    },
+    title: {
+      type: String,
+      default: null
+    },
+    ssddetial: {
+      type: Array
+    }
+  },
+  components: { Piechart },
+  computed: {},
+  methods: {
+    setPiecolor(sensorname) {
+      let color = "";
+      switch (sensorname) {
+        case "WriteLength 2K":
+          color = "#CDF54B";
+          break;
+        case "WriteLength 4K":
+          color = "#FF96B4";
+          break;
+        case "WriteLength 8K":
+          color = "#FFFF5F";
+          break;
+        case "WriteLength 16K":
+          color = "#82BDFB";
+          break;
+        case "WriteLength 32K":
+          color = "#C8A5FF";
+          break;
+        case "WriteLength 64K":
+          color = "#E3474D";
+          break;
+        case "WriteLength 128K":
+          color = "#F5EA13";
+          break;
+        case "WriteLength Other":
+          color = "#4175DE";
+          break;
+        case "ReadLength 2K":
+          color = "#CDF54B";
+          break;
+        case "ReadLength 4K":
+          color = "#FF96B4";
+          break;
+        case "ReadLength 8K":
+          color = "#FFFF5F";
+          break;
+        case "ReadLength 16K":
+          color = "#82BDFB";
+          break;
+        case "ReadLength 32K":
+          color = "#C8A5FF";
+          break;
+        case "ReadLength 64K":
+          color = "#E3474D";
+          break;
+        case "ReadLength 128K":
+          color = "#F5EA13";
+          break;
+        case "ReadLength Other":
+          color = "#4175DE";
+          break;
+      }
+      return color;
+    },
+    async getsensoridListdata(ssddata) {
+      let beginTs = moment().format("YYYY-MM-DD 00:00:00.000");
+      let endTs = moment().format("YYYY-MM-DD 23:59:59.000");
+      let allsensordata = [];
+      for (let sensor of this.sensorList) {
+        let sensorId = "/Disk/" + ssddata.DiskName + "/" + sensor;
+        await this.getHisData(
+          ssddata.AgentId,
+          "SSDStatus",
+          sensorId,
+          beginTs,
+          endTs,
+          "1"
+        ).then(function(xhr) {
+          allsensordata.push(xhr.data.itemList);
+        });
+      }
+      return allsensordata;
+    },
+    async setpiedata() {
+      for (let i = 0; i < this.sensorList.length; i++) {
+        let labels = [];
+        let backgroundColor = [];
+        let borderColor = [];
+        let data = [];
+        let sensordata = null;
+        await this.getsensoridListdata(this.allrowpiedata[i]).then(function(
+          res
+        ) {
+          sensordata = res;
+        });
+        // for API的資料
+        for (let ssdrow of sensordata) {
+          let barcolor = this.setPiecolor(ssdrow[0].n);
+          labels.push(ssdrow[0].n);
+          data.push(ssdrow[0].v);
+          backgroundColor.push(barcolor);
+          borderColor.push(barcolor);
+        }
+        // 創建charts圖形的必要屬性
+        this.$set(this.ssdoptions, i, {});
+        this.ssdoptions[i].chartData = Object.assign(
+          {},
+          this.ssdoptions[i].chartData,
+          {
+            labels: labels,
+            borderSkipped: "bottom",
+            datasets: [
+              {
+                fill: true,
+                label: labels,
+                backgroundColor: backgroundColor,
+                borderColor: borderColor,
+                data: data
+              }
+            ]
+          }
+        );
+        this.ssdoptions[i].chartOptions = Object.assign(
+          {},
+          this.ssdoptions[i].chartOptions,
+          {
+            title: {
+              text: this.sensorList[i].title,
+              display: true,
+              position: "top"
+            },
+            legend: {
+              display: false
+            },
+            rotation: -0.7 * Math.PI
+          }
+        );
+      }
+    }
+  },
+  created() {
+    this.setpiedata();
+  },
+  data() {
+    return {
+      // 固定按照功能分配API
+      ssdoptions: [],
+      sensorList: [
+        {
+          title: "Total Sequential & Random Read/Write",
+          List: [
+            "ReadLength 2K",
+            "ReadLength 4K",
+            "ReadLength 8K",
+            "ReadLength 16K",
+            "ReadLength 32K",
+            "ReadLength 64K",
+            "ReadLength 128K",
+            "ReadLength Other"
+          ]
+        },
+        {
+          title: "4K Alignment",
+          List: [
+            "WriteLength 2K",
+            "WriteLength 4K",
+            "WriteLength 8K",
+            "WriteLength 16K",
+            "WriteLength 32K",
+            "WriteLength 64K",
+            "WriteLength 128K",
+            "WriteLength other"
+          ]
+        },
+        {
+          title: "Length of Read",
+          List: [
+            "ReadLength 2K",
+            "ReadLength 4K",
+            "ReadLength 8K",
+            "ReadLength 16K",
+            "ReadLength 32K",
+            "ReadLength 64K",
+            "ReadLength 128K",
+            "ReadLength Other"
+          ]
+        },
+        {
+          title: "Length of Write",
+          List: [
+            "WriteLength 2K",
+            "WriteLength 4K",
+            "WriteLength 8K",
+            "WriteLength 16K",
+            "WriteLength 32K",
+            "WriteLength 64K",
+            "WriteLength 128K",
+            "WriteLength other"
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
